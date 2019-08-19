e2264c7d77aea7798e1f927b27fc24cd736bcd98
Worome <41673409+Worome@users.noreply.github.com>
2019-08-19 10:39:08 +0200

    Terminado

--- app/src/main/java/com/trianacodes/script/imagebutton/MainActivity.java
+++ app/src/main/java/com/trianacodes/script/imagebutton/MainActivity.java
@@ -2,6 +2,8 @@ package com.trianacodes.script.imagebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ib1(View view){
+
+        Toast.makeText(this, "Botón del niño",Toast.LENGTH_LONG).show();
+
+    }
+
+    public void ib2(View view){
+
+        Toast.makeText(this, "Botón del teléfono",Toast.LENGTH_LONG).show();
+
+    }
+
 }
